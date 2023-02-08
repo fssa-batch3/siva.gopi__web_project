48a3c387956ca19d1042a73080fb21f1b09b44ef
deemoseenu <siva.gopi@fwsa.freshworks.com>
2023-02-08 15:13:05 +0530

    add some page..

---
+++ main.js
@@ -0,0 +1,57 @@
+// // object
+
+// const name_list = [
+//     {
+//         "name": "Arun",
+//         "image": "https://ui-avatars.com/api/?name=Arun",
+//         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//     },
+//     {
+//         "name": "Ajith",
+//         "image": "https://ui-avatars.com/api/?name=Ajith",
+//         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//     },
+//     {
+//         "name": "Bala",
+//         "image": "https://ui-avatars.com/api/?name=Bala",
+//         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//     },
+//     {
+//         "name": "Deva",
+//         "image": "https://ui-avatars.com/api/?name=Deva",
+//         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit.Est unde quas laborum accusantium molestias velit mollitia alias quia earum repudiandae odio molestiae modi possimus aut, nisi magni quos.Dicta, cupiditate."
+//     }
+
+// ]
+
+// let n=5;
+// let str="";
+// for (let i=1; i<=n; i++){
+//     for (let j=n; j>n-i; j--){
+//     str+=j+" ";
+    
+// }
+// str+="\n"
+
+// }
+// console.log(str)
+
+
+// let n=5;
+// let str="";
+// for (let i=1; i<=n; i++){
+//    str+=i+" ";
+
+// console.log(str)
+// }
+
+let a = 5;
+let b = 3;
+
+    a = a+b;
+    b = a-b;
+    a = b-a;
+
+    console.log(a);
+    console.log(b);
+
